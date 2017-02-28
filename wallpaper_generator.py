b148948cc058cfacb8e24f741cdfe7a7edd3fed6
Shy <shyamal@ruparel.co>
2017-02-27 19:00:39 -0500

    Cleaned up line drawing code to be readable.

--- wallpaper_generator.py
+++ wallpaper_generator.py
@@ -5,41 +5,56 @@ x = 2560
 y = 1600
 
 #Colours
-colour1 = (197,60,2,0)
-colour2 = (38,90,143,0)
-colour3 = (253,238,0,0)
+colour2 = (197,60,2,0)
+colour1 = (38,90,143,0)
+colour3 = (255,255,255,0)
 background = (53,53,53)
 
-logo = Image.open("logo.png")
+logo = Image.open("logoSR.png")
 
 cx = x/4
 cy = y/3
 
-width = 50
-cr1 = 400
-linebuffer = 150
+
+cr1 = 450
+width = cr1/5
+cwidth = cr1/8
+
+linebuffer = 250
+spacing = width + 55
 
 image = Image.new('RGB', (x, y),background)
 draw = ImageDraw.Draw(image)
 
-draw.line([(0-linebuffer, ((cy*2) - (2 * width))+linebuffer), (cx+cy,0 - (2* width))], colour1, width)
-draw.line([((0)-linebuffer, cy*2+linebuffer), ((cx+cy+linebuffer),0-linebuffer)], colour2, width)
-draw.line([(0-linebuffer, ((cy*2) + (2 * width))+linebuffer), (cx+cy+linebuffer , 0 + (2* width)-linebuffer)], colour3, width)
+# draw.line([((0)-linebuffer, cy*2+linebuffer), ((cx+cy+linebuffer),0-linebuffer)], colour2, width)
 
-cr2 = cr1 - width
-cr3 = cr2 - width
-cr4 = cr3 - width
+endy = - linebuffer
+endx = cx + (cy-endy)
+
+startx = - linebuffer
+starty = cy + (cx-startx)
+
+draw.line([(startx,starty-spacing),(endx,endy-spacing)], colour1, width)
+draw.line([(startx,starty),(endx,endy)], colour2, width)
+draw.line([(startx,starty+spacing),(endx,endy+spacing)], colour3, width)
+
+# draw.line([((0)-linebuffer, cy*2+linebuffer), ((cx+cy+linebuffer),0-linebuffer)], colour2, width)
+# draw.line([(0-linebuffer, ((cy*2) + (2 * width))+linebuffer), (cx+cy+linebuffer , 0 + (2* width)-linebuffer)], colour3, width)
+
+cr2 = cr1 - cwidth
+cr3 = cr2 - cwidth
+cr4 = cr3 - cwidth
 
 logowidth,logoheight=logo.size
-logo = ImageOps.fit(logo, (cr1 , cr1*logoheight/logowidth), Image.ANTIALIAS)
+logo = ImageOps.fit(logo, ((cr1+50) , (cr1+50)*logoheight/logowidth), Image.ANTIALIAS)
 logowidth,logoheight=logo.size
 
 draw.ellipse((cx-cr1, cy-cr1, cx+cr1, cy+cr1), fill=colour1)
 draw.ellipse((cx-cr2, cy-cr2, cx+cr2, cy+cr2), fill=colour2)
-draw.ellipse((cx-cr3, cy-cr3, cx+cr3, cy+cr3), fill=colour3)
+draw.ellipse((cx-cr3, cy-cr3, cx+cr3, cy+cr3), fill=background)
 draw.ellipse((cx-cr4, cy-cr4, cx+cr4, cy+cr4), fill=background)
 
 #Passing logo twice since it indicates a mask that will be used to paste the image. If you pass a image with transparency, then the alpha channel is used as mask.
 image.paste(logo, (cx - logowidth/2,cy - logoheight/2), logo)
 
-image.save("output.png")
+image.save("outputSR.png")
